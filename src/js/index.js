a144406da87ab7a0e4d3c5f2e48b4262ca150352
alogan5201 <andrewthomaslogan@gmail.com>
2022-04-07 12:25:20 -0400

    update

--- src/js/index.js
+++ src/js/index.js
@@ -6,26 +6,32 @@
  * all the paths within Dir in globals.js
  */
 
-import dompurify from 'dompurify'
-import { initializeApp } from 'firebase/app';
+import dompurify from "dompurify";
+import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
-import { getAuth, signInWithRedirect, getRedirectResult, signOut, GoogleAuthProvider, onAuthStateChanged, connectAuthEmulator } from "firebase/auth";
+import {
+  getAuth,
+  signInWithRedirect,
+  getRedirectResult,
+  signOut,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  connectAuthEmulator,
+} from "firebase/auth";
 
-import 'picturefill'
-import 'utils/errors'
+import "picturefill";
+import "utils/errors";
 
+let url = window.location.href;
 
-
-let url = window.location.href
-
- var firebaseConfig = {
+var firebaseConfig = {
   apiKey: "AIzaSyBCU8RRxV3qaSyxOgc4ObSWmUhlfnJsYTo",
   authDomain: "geotools-bc75a.firebaseapp.com",
   projectId: "geotools-bc75a",
   storageBucket: "geotools-bc75a.appspot.com",
   messagingSenderId: "106157954659",
   appId: "1:106157954659:web:3e189110236a2138438a56",
-  measurementId: "G-Z6GK19K3L0"
+  measurementId: "G-Z6GK19K3L0",
 };
 
 const app = initializeApp(firebaseConfig);
@@ -34,14 +40,13 @@ const auth = getAuth();
 
 const googleProvider = new GoogleAuthProvider();
 const db = getDatabase();
-
+/*
 if (location.hostname === "localhost") {
   // Point to the RTDB emulator running on localhost.
 connectAuthEmulator(auth, "http://localhost:9099");
 }
-
-if(window.location.href.includes('login') ){
-
+*/
+if (window.location.href.includes("login")) {
   const App = function _App() {
     return `
 
@@ -63,156 +68,101 @@ if(window.location.href.includes('login') ){
   };
   App.state = {
     loading: false,
-    class: 'd-none',
+    class: "d-none",
     count: 0,
     toggleState: () => {
       setState(() => {
-        App.state.loading  = App.state.loading ? false : true
-
+        App.state.loading = App.state.loading ? false : true;
       });
-    }
+    },
   };
-
-
 
   function toggleLoading() {
-
-  if( App.state.loading == true){
-    App.state.loading = false;
-    App.state.class = 'd-none'
-
-  } else {
-    App.state.loading = true
-    App.state.class = ''
-
+    if (App.state.loading == true) {
+      App.state.loading = false;
+      App.state.class = "d-none";
+    } else {
+      App.state.loading = true;
+      App.state.class = "";
+    }
   }
 
-  };
+  $("#google-sign-in").on("click", function (e) {
+    e.preventDefault();
 
+    $("main").addClass("d-none");
 
-
-$('#google-sign-in').on('click', function (e) {
-
-e.preventDefault()
-
-$('main').addClass('d-none')
-
-  $('#spinner-box').removeClass('d-none')
-signInWithRedirect(auth, googleProvider);
-});
-onAuthStateChanged(auth,  (user) => {
-
-
-  if (user) {
-
-
-
-
-  getRedirectResult(auth)
-  .then((result) => {
-
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const token = credential.accessToken;
-    const user = result.user;
-
-
-
-
-  }).catch((error) => {
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // The email of the user's account used.
-    const email = error.email;
-    // The AuthCredential type that was used.
-    const credential = GoogleAuthProvider.credentialFromError(error);
-    // ...
-  })
-  .finally(() => {
-
-
-    window.location.replace("/");
+    $("#spinner-box").removeClass("d-none");
+    signInWithRedirect(auth, googleProvider);
   });
-  } else {
-
-$('#spinner-box').addClass('d-none')
-$('main').removeClass('d-none')
-      console.log(' signed out')
-  }
-});
-
-        import(/* webpackChunkName: "lazy-module.login" */ './lazy-module').then(module => {
-            const login = module.default
-
-            login()
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      getRedirectResult(auth)
+        .then((result) => {
+          const credential = GoogleAuthProvider.credentialFromResult(result);
+          const token = credential.accessToken;
+          const user = result.user;
         })
+        .catch((error) => {
+          // Handle Errors here.
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          // The email of the user's account used.
+          const email = error.email;
+          // The AuthCredential type that was used.
+          const credential = GoogleAuthProvider.credentialFromError(error);
+          // ...
+        })
+        .finally(() => {
+          window.location.replace("/");
+        });
+    } else {
+      $("#spinner-box").addClass("d-none");
+      $("main").removeClass("d-none");
+      console.log(" signed out");
+    }
+  });
+
+  import(/* webpackChunkName: "lazy-module.login" */ "./lazy-module").then(
+    (module) => {
+      const login = module.default;
+
+      login();
+    }
+  );
 }
 
-
-
-
-
-window.addEventListener('DOMContentLoaded', (event) => {
-$('#testBtn').on('click', function () {
-   var myModal = new bootstrap.Modal(document.getElementById('modalSignOut'))
- myModal.toggle()
-});
-
-onAuthStateChanged(auth,  (user) => {
-
-  if (user) {
-
-  $("#sign-in-elm").css("display", "none");
-  $("#sign-out-elm").css("display", "block");
-
-
-  } else {
-
-
-  $("#sign-out-elm").css("display", "none");
-  $("#sign-in-elm").css("display", "block");
-
-
-
-
-
-  }
-});
-
-
-
-$('#sign-out').on('click', async function (e) {
-  e.preventDefault()
-  signOut(auth).then(() => {
-    var myModal = new bootstrap.Modal(document.getElementById('modalSignOut'))
-    myModal.toggle()
-}).catch((error) => {
-
-});
-
-
-
-});
-
-
-
-  $('#user-btn').on('click', function (e) {
-    e.preventDefault()
+window.addEventListener("DOMContentLoaded", (event) => {
+  $("#testBtn").on("click", function () {
+    var myModal = new bootstrap.Modal(document.getElementById("modalSignOut"));
+    myModal.toggle();
   });
 
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      $("#sign-in-elm").css("display", "none");
+      $("#sign-out-elm").css("display", "block");
+    } else {
+      $("#sign-out-elm").css("display", "none");
+      $("#sign-in-elm").css("display", "block");
+    }
+  });
 
+  $("#sign-out").on("click", async function (e) {
+    e.preventDefault();
+    signOut(auth)
+      .then(() => {
+        var myModal = new bootstrap.Modal(
+          document.getElementById("modalSignOut")
+        );
+        myModal.toggle();
+      })
+      .catch((error) => {});
+  });
 
-// Movie Directory
+  $("#user-btn").on("click", function (e) {
+    e.preventDefault();
+  });
 
-
-
-
-
-
-
-
-
-
-
-
+  // Movie Directory
 });
