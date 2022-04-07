f858dd367dee27e4f473fc28f9ad855ce0072b93
alogan5201 <andrewthomaslogan@gmail.com>
2022-04-07 13:02:18 -0400

    update

---
+++ dist/js/lazy-module.login.34a7a6518821c61e955466ac42f95cdc.js
@@ -0,0 +1,9 @@
+/*!
+* GeoTools
+* Boilerplate for a Static website using EJS and SASS
+* example.com
+* @author Brenton Cozby -- https://brentoncozby.com
+* Copyright 2022. MIT Licensed.
+*/
+
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{38:function(module,__webpack_exports__,__webpack_require__){"use strict";eval("__webpack_require__.r(__webpack_exports__);\nconsole.log('This file was lazyLoaded.');\n/* harmony default export */ __webpack_exports__[\"default\"] = (function () {\n  console.log('foo.');\n});\nsetTimeout(() => {\n  __webpack_require__.e(/* import() | login.lazy */ 9).then(__webpack_require__.bind(null, 40)).then(module => {\n    const login = module.default;\n    login();\n  });\n}, 2000);\n\n//# sourceURL=webpack:///./src/js/lazy-module.js?")}}]);
